fda3c028b5241c844e24956299006315df65aab0
ccawis <4500754945@estudante.sed.sc.gov.br>
2024-08-29 20:17:51 -0300

    1° commit - ROTAS

---
+++ src/pages/Login.jsx
@@ -0,0 +1,12 @@
+import { Header } from "../components/Header"
+import { Footer } from "../components/Footer"
+
+export const Login = () => {
+    return (
+        <div>
+            <Header />
+            <h1>Page Login</h1>
+            <Footer />
+        </div>
+    )
+}
