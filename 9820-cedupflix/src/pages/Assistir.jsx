a7d58f7745147ccc133eb3de6c1662935008895d
Luis <monteirosales51@gmail.com>
2024-08-29 20:42:36 -0300

    1º commit - ROTAS

---
+++ 9820-cedupflix/src/pages/Assistir.jsx
@@ -0,0 +1,12 @@
+import { Header } from "../components/Header"
+import { Footer } from "../components/Footer"
+
+export const Assistir = () => {
+    return (
+        <div>
+            <Header />
+            <h1>Page Watch</h1>
+            <Footer />
+        </div>
+    )
+}
