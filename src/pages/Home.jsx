5233012075d5f8108f8d5777e185c78cabf32ee1
Lucas2767 <lucasalmeida89012@gmail.com>
2024-08-30 21:38:11 -0300

    [Create] form

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import { Header } from "../components/Header"
 import { Footer } from "../components/Footer"
+import { FormUser } from "../components/FormUser"
 
 export const Home = () => {
     return (
@@ -7,6 +8,7 @@ export const Home = () => {
             <Header />
             <h1>Page Home</h1>
             <Footer />
+            <FormUser />
         </div>
     )
 }
