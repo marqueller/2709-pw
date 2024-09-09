791947a08510b07615ab6a2243fac61c61f1d52c
luis <monteirosales51@gmail.com>
2024-09-09 20:02:12 -0300

    4° commit - images

---
+++ src/pages/Assistir.jsx
@@ -0,0 +1,13 @@
+import { Header } from "../components/Header"
+import { Footer } from "../components/Footer"
+import './style.css'
+
+export const Assistir = () => {
+    return (
+        <div className="background1">
+            <Header />
+            <h1>Page Watch</h1>
+            <Footer />
+        </div>
+    )
+}
---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import { Header } from "../components/Header"
+import { Footer } from "../components/Footer"
+import { FormUser } from "../components/FormUser"
+import './style.css'
+
+
+export const Home = () => {
+    return (
+        <div className='background2'>
+            <Header />
+            <h1>Page Home</h1>
+            <Footer />
+            <FormUser />
+        </div>
+    )
+}
