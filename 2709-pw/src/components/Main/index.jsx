ef72967771ee19f2a34a8aba841574b4c57f3890
Camile <brugnagoca@gmail.com>
2024-09-10 20:45:38 -0300

    4° commit - Images

---
+++ 2709-pw/src/components/Main/index.jsx
@@ -0,0 +1,9 @@
+import styles from "./Main.module.css";
+
+export const Main = () => {
+    return (
+        <div className={styles.img}>
+
+        </div>
+    )
+}
---
+++ 2709-pw/src/routes/routes.jsx
@@ -0,0 +1,27 @@
+// // src/router/router.js
+// import { createBrowserRouter } from 'react-router-dom';
+// import App from '../App.jsx';
+// import { Home } from '../pages/Home.jsx';
+// import { Assistir } from '../pages/Assistir.jsx';
+// import { Login } from '../pages/Login.jsx';
+
+// const router = createBrowserRouter([
+//     {
+//         path: "/",
+//         element: <App />
+//     },
+//     {
+//         path: "/home",
+//         element: <Home />
+//     },
+//     {
+//         path: "/assistir",
+//         element: <Assistir />
+//     },
+//     {
+//         path: "/login",
+//         element: <Login />
+//     },
+// ]);
+
+// export default router;
