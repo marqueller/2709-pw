73800fd11827d6337bbdcf244019cff169f95cd9
lucasvelozo1 <velozolucas157@gmail.com>
2024-09-10 19:13:41 -0300

    4°commit

---
+++ src/pages/Assistir.jsx
@@ -0,0 +1,14 @@
+import { Header } from "../components/Header"
+import { Footer } from "../components/Footer"
+import './Assistir.css'
+export const Assistir = () => {
+    return (
+        <div className="background">
+            <div>
+                <Header />
+                <h1>Page Watch</h1>
+                <Footer />
+            </div>
+        </div>
+    )
+}
