86da105d957dcbefbdb29a959273a951a040f3d8
milenasvarela <milenadesouzavarela@gmail.com>
2024-09-12 19:37:57 -0300

    5° commit

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,11 @@
+import styles from "./Footer.module.css";
+
+export const Footer = () => {
+    return (
+        <div>
+            <footer className={styles.footer}>
+                <h2>CedupFlix &copy; Desenvolvido por Milena Varela</h2>
+            </footer>
+        </div>
+    );
+}
